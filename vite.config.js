56eb1c8211c42abbb963c2e5fe10d6786c35285a
Farizgeloz <farizgeloz26@gmail.com>
2025-12-23 17:17:04 +0700

    two commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //base: '/frontend-opendata/', // <- penting untuk GitHub Pages
+  base: '/opendata/',
   server: {
     port: 3001,
     strictPort: true
