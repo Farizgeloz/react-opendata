f7c1d0813a1ae6481973fed9740a3f39cada9292
Farizgeloz <farizgeloz26@gmail.com>
2025-12-23 17:53:37 +0700

    two commit23

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -9,7 +9,8 @@ const api_url_satudata = axios.create({
 
 const api_url_satuadmin = axios.create({
   /* baseURL: "http://localhost:3010/", */
-  baseURL: "https://api-satu.mataprabulinggih.net/",
+  /* baseURL: "https://api-satu.mataprabulinggih.net/", */
+  baseURL: "http://103.170.105.216/api/",
   headers: {
     "Content-Type": "application/json",
   },
@@ -17,7 +18,8 @@ const api_url_satuadmin = axios.create({
 
 const api_url_satuadmin_create = axios.create({
   /* baseURL: "http://localhost:3010/", */
-  baseURL: "https://api-satu.mataprabulinggih.net/",
+  /* baseURL: "https://api-satu.mataprabulinggih.net/", */
+  baseURL: "http://103.170.105.216/api/",
 });
 
 export { api_url_satudata, api_url_satuadmin, api_url_satuadmin_create };
